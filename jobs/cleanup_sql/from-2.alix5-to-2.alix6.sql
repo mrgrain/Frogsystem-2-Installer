934b330b1d21cd63c54acbbcf8c88dcb39f98f96
Sweil <mail@sweil.de>
2013-07-07 23:19:52 +0200

    cleanup from-alix5
    Signed-off-by: Sweil <mail@sweil.de>

--- jobs/cleanup_sql/from-2.alix5-to-2.alix6.sql
+++ jobs/cleanup_sql/from-2.alix5-to-2.alix6.sql
@@ -1,1 +1,15 @@
-DROP TABLE IF EXISTS `{..pref..}blabla`;
+DROP TABLE `{..pref..}articles_config`;
+DROP TABLE `{..pref..}captcha_config`;
+DROP TABLE `{..pref..}dl_config`;
+DROP TABLE `{..pref..}global_config`;
+DROP TABLE `{..pref..}iplist`;
+DROP TABLE `{..pref..}news_comments`;
+DROP TABLE `{..pref..}news_config`;
+DROP TABLE `{..pref..}partner_config`;
+DROP TABLE `{..pref..}player_config`;
+DROP TABLE `{..pref..}poll_config`;
+DROP TABLE `{..pref..}press_config`;
+DROP TABLE `{..pref..}screen_config`;
+DROP TABLE `{..pref..}screen_random_config`;
+DROP TABLE `{..pref..}search_config`;
+DROP TABLE `{..pref..}user_config`;
