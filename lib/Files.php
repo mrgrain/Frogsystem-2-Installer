b94c299a3e6c2e349cc081f321ae641fad738fa8
Sweil <mail@sweil.de>
2013-07-21 00:08:07 +0200

    add overwrite option to context changed is_writable and is_readable to use fopen
    Signed-off-by: Sweil <mail@sweil.de>

--- lib/Files.php
+++ lib/Files.php
@@ -11,20 +11,19 @@
 class Files {
 
     // set url-wrapper for filesystem functions
-    private static $uw = '';
-    private static $current_dir = '';
-
+    protected static $uw = '';
     public static function setUrlWrapper($_uw, $_current_dir) {
         self::$uw = $_uw;
         self::$current_dir = $_current_dir;
     }
 
-    private static function getPath($path) {
+    // get path with current dir prefixed
+    protected static function getPath($path) {
         return self::$uw.self::$current_dir.DIRECTORY_SEPARATOR.$path;
     }
 
     // is file wrapper
-    private static function isFileWrapper() {
+    protected static function isFileWrapper() {
         if (empty(self::$uw)) {
             return true;
         }
@@ -35,11 +34,27 @@ class Files {
     }
 
     // pseudo callStatic for functions
-    private static function call($name, $arguments, $keys) {
+    protected static function call($name, $arguments, $keys, $contexts = array(), $use_defaults = array()) {
         // prepend url-wrapper
         foreach($keys as $key) {
             $arguments[$key] = self::getPath($arguments[$key]);
         }
+
+        // set context to overwrite
+        $options = array('ftp' => array('overwrite' => true));
+        foreach($contexts as $context) {
+            if (isset($arguments[$context])) {
+                stream_context_set_option($arguments[$context], $options);
+            } else {
+                $arguments[$key] = stream_context_create($options);
+                foreach($use_defaults as $key => $default) {
+                    if (!isset($arguments[$key])) {
+                        $arguments[$key] = $default;
+                    }
+                }
+            }
+        }
+
         // call function
         return call_user_func_array($name, $arguments);
     }
@@ -47,19 +62,19 @@ class Files {
 
     // map filesystem functions
     public static function copy() {
-        return self::call('copy', func_get_args(), array(0,1));
+        return self::call('copy', func_get_args(), array(0,1), array(2));
     }
     public static function file_exists() {
         return self::call('file_exists', func_get_args(), array(0));
     }
     public static function file_get_contents() {
-        return self::call('file_get_contents', func_get_args(), array(0));
+        return self::call('file_get_contents', func_get_args(), array(0), array(2), array(1 => false));
     }
     public static function file_put_contents() {
-        return self::call('file_put_contents', func_get_args(), array(0));
+        return self::call('file_put_contents', func_get_args(), array(0), array(3), array(2 => 0));
     }
     public static function file() {
-        return self::call('file', func_get_args(), array(0));
+        return self::call('file', func_get_args(), array(0), array(2), array(1 => 0));
     }
     public static function fileatime() {
         return self::call('fileatime', func_get_args(), array(0));
@@ -89,7 +104,10 @@ class Files {
         return self::call('filetype', func_get_args(), array(0));
     }
     public static function fopen() {
-        return self::call('fopen', func_get_args(), array(0));
+        return self::call('fopen', func_get_args(), array(0), array(3), array(2 => false));
+    }
+    public static function fclose() {
+        return call_user_func_array('fclose', func_get_args());
     }
     public static function is_dir() {
         return self::call('is_dir', func_get_args(), array(0));
@@ -103,17 +121,26 @@ class Files {
     public static function is_link() {
         return self::call('is_link', func_get_args(), array(0));
     }
-    public static function is_readable() {
-        return self::call('is_readable', func_get_args(), array(0));
+    public static function is_readable($filename) {
+        $file = self::fopen($filename, 'r');
+        if ($file !== false) {
+            self:fclose($file);
+            return true;
+        }
+        return false;
     }
-    public static function is_writable() {
-        return self::call('is_writable', func_get_args(), array(0));
+    public static function is_writable($filename) {
+        if (self::is_dir($filename)) {
+            return self::is_writable_dir($filename);
+        } else {
+            return self::is_writable_file($filename);
+        }
     }
     public static function lstat() {
         return self::call('lstat', func_get_args(), array(0));
     }
     public static function mkdir() {
-        return self::call('mkdir', func_get_args(), array(0));
+        return self::call('mkdir', func_get_args(), array(0), array(3), array(1 => 0777, 2 => false));
     }
     public static function move_uploaded_file() { //?
         return self::call('move_uploaded_file', func_get_args(), array(1));
@@ -122,10 +149,10 @@ class Files {
         return self::call('readfile', func_get_args(), array(0));
     }
     public static function rename() {
-        return self::call('rename', func_get_args(), array(0,1));
+        return self::call('rename', func_get_args(), array(0,1), array(1));
     }
     public static function rmdir() {
-        return self::call('rmdir', func_get_args(), array(0));
+        return self::call('rmdir', func_get_args(), array(0), array(1));
     }
     public static function stat() {
         return self::call('stat', func_get_args(), array(0));
@@ -134,7 +161,7 @@ class Files {
         return self::call('touch', func_get_args(), array(0));
     }
     public static function unlink() {
-        return self::call('unlink', func_get_args(), array(0));
+        return self::call('unlink', func_get_args(), array(0), array(1));
     }
 
     // map directory functions
@@ -153,6 +180,33 @@ class Files {
     }
     public static function closedir() {
         return self::call('closedir', func_get_args(), array());
+    }
+
+
+    // helper functions
+    protected static function is_writable_file($filename) {
+        $unlink = self::file_exists($filename)?true:false;
+        $file = self::fopen($filename, 'ab');
+        if ($file !== false) {
+            self:fclose($file);
+            self::unlink($filename);
+            return true;
+        }
+        return false;
+    }
+
+    protected static function is_writable_dir($filename) {
+        do {
+            $path = $filename.DIRECTORY_SEPERATOR.mt_rand(0, 9999999);
+        } while (self::file_exists($path));
+
+        $file = self::fopen($path, 'ab');
+        if ($file !== false) {
+            self:fclose($file);
+            self::unlink($path);
+            return true;
+        }
+        return false;
     }
 }
 ?>
